322184792c26d8e3fe798ff9487f49e1d627ce87
cjaem <84785975+jaem-code@users.noreply.github.com>
2023-07-14 16:09:02 +0900

    Stage4/ 한글 주석 추가

--- Meal Planner (Kotlin)/task/src/mealplanner/Main.kt
+++ Meal Planner (Kotlin)/task/src/mealplanner/Main.kt
@@ -1,151 +1,166 @@
-import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Statement
 
-// 식사를 나타내는 클래스를 정의합니다. 이 클래스는 식사의 종류, 이름, 그리고 재료 리스트를 속성으로 갖습니다.
+// 식사의 카테고리, 이름, 재료를 담는 데이터 클래스
 data class Meal(val category: String, val name: String, val ingredients: List<String>)
 
 fun main() {
+    // SQLite 데이터베이스 연결
     val connection = DriverManager.getConnection("jdbc:sqlite:meals.db")
+    // SQL 구문을 실행하는 객체 생성
     val statement = connection.createStatement()
+    // meals 테이블 생성 (이미 존재하는 경우 무시)
+    statement.execute("CREATE TABLE IF NOT EXISTS meals(meal_id INTEGER PRIMARY KEY, category TEXT, meal TEXT)")
+    // ingredients 테이블 생성 (이미 존재하는 경우 무시)
+    statement.execute("CREATE TABLE IF NOT EXISTS ingredients(ingredient_id INTEGER PRIMARY KEY, meal_id INTEGER, ingredient TEXT)")
 
-    // Create the meals table if it doesn't exist
-    statement.execute(
-        """
-        CREATE TABLE IF NOT EXISTS meals (
-            meal_id INTEGER PRIMARY KEY,
-            category TEXT,
-            meal TEXT
-        )
-        """
-    )
-
-    // Create the ingredients table if it doesn't exist
-    statement.execute(
-        """
-        CREATE TABLE IF NOT EXISTS ingredients (
-            ingredient_id INTEGER PRIMARY KEY,
-            ingredient TEXT,
-            meal_id INTEGER,
-            FOREIGN KEY (meal_id) REFERENCES meals (meal_id)
-        )
-        """
-    )
-
-    // 무한 루프를 시작합니다. 사용자가 'exit'를 입력하면 루프를 종료합니다.
+    // 무한 루프로 사용자 입력 처리
     while (true) {
         println("What would you like to do (add, show, exit)?")
-        when (readLine()) {
-            "add" -> addMeal(statement)
-            "show" -> showMeals(statement)
+        val action = readLine()
+
+        when (action) {
+            "add" -> addMeal(statement)  // 식사 추가
+            "show" -> showMeals(statement)  // 식사 조회
             "exit" -> {
                 println("Bye!")
-                break
+                break  // 프로그램 종료
             }
-            else -> println("Invalid option. Please try again.")
+            else -> println("No meals found.")
         }
     }
 
+    // 데이터베이스 연결 종료
     connection.close()
 }
 
 fun addMeal(statement: Statement) {
-    val category = getCategory()
-    val name = getName()
-    val ingredients = getIngredients()
+    println("Which meal do you want to add (breakfast, lunch, dinner)?")
+    val category = readLine()
 
-    statement.execute("INSERT INTO meals (category, meal) VALUES ('$category', '$name')")
-    val mealId = statement.generatedKeys.getInt(1)
+    if (category !in listOf("breakfast", "lunch", "dinner")) {
+        println("Wrong meal category! Choose from: breakfast, lunch, dinner.")
+        return
+    }
 
-    for (ingredient in ingredients) {
-        statement.execute("INSERT INTO ingredients (ingredient, meal_id) VALUES ('$ingredient', $mealId)")
+    println("Input the meal's name:")
+    val meal = readLine()
+
+    println("Input the ingredients:")
+    val ingredients = readLine()
+
+    // meals 테이블에서 가장 큰 meal_id를 찾고 1을 더해 새로운 meal_id 생성
+    val resultSet = statement.executeQuery("SELECT MAX(meal_id) AS max_id FROM meals")
+    val mealId = resultSet.getInt("max_id") + 1
+
+    // 식사를 meals 테이블에 추가
+    statement.execute("INSERT INTO meals VALUES($mealId, '$category', '$meal')")
+
+    if (ingredients != null) {
+        val ingredientList = ingredients.split(",")
+        // 각 재료를 ingredients 테이블에 추가
+        for (ingredient in ingredientList) {
+            statement.execute("INSERT INTO ingredients(meal_id, ingredient) VALUES($mealId, '${ingredient.trim()}')")
+        }
     }
 
     println("The meal has been added!")
 }
 
 fun showMeals(statement: Statement) {
+    println("Which category do you want to print (breakfast, lunch, dinner)?")
+    val category = readLine()
+
+    if (category !in listOf("breakfast", "lunch", "dinner")) {
+        println("Wrong meal category! Choose from: breakfast, lunch, dinner.")
+        return
+    }
+
+    // 입력받은 카테고리에 해당하는 식사와 그 재료를 조회
     val mealsResult = statement.executeQuery(
         """
-        SELECT meals.category, meals.meal, ingredients.ingredient
+        SELECT meals.meal, ingredients.ingredient
         FROM meals
         JOIN ingredients ON meals.meal_id = ingredients.meal_id
+        WHERE meals.category = '$category'
+        ORDER BY meals.meal_id
         """
     )
 
-    var currentCategory = ""
-    var currentMeal = ""
-    var ingredients = mutableListOf<String>()
+    var previousMeal = ""
+    var mealFound = false
 
     while (mealsResult.next()) {
-        val category = mealsResult.getString("category")
-        val meal = mealsResult.getString("meal")
+        val mealName = mealsResult.getString("meal")
         val ingredient = mealsResult.getString("ingredient")
 
-        if (category != currentCategory || meal != currentMeal) {
-            if (currentCategory.isNotEmpty() && currentMeal.isNotEmpty()) {
-                printMeal(currentCategory, currentMeal, ingredients)
+        if (mealName != previousMeal) {
+            if (!mealFound) {
+                // 카테고리 출력
+                println("Category: $category")
+                mealFound = true
             }
-            currentCategory = category
-            currentMeal = meal
-            ingredients = mutableListOf()
+            if (previousMeal.isNotEmpty()) {
+                println()
+            }
+            // 식사 이름 출력
+            println("Name: $mealName")
+            // 재료 리스트 헤더 출력
+            println("Ingredients:")
+            previousMeal = mealName
         }
 
-        ingredients.add(ingredient)
+        // 재료 출력
+        println(ingredient)
     }
 
-    if (currentCategory.isNotEmpty() && currentMeal.isNotEmpty()) {
-        printMeal(currentCategory, currentMeal, ingredients)
-    }
-
-    if (currentCategory.isEmpty() && currentMeal.isEmpty()) {
-        println("No meals saved. Add a meal first.")
+    if (!mealFound) {
+        println("No meals found.")
     }
 }
 
+// 식사 정보 출력 함수
 fun printMeal(category: String, meal: String, ingredients: List<String>) {
-    println("\nCategory: $category")
+    if (category != "") {
+        println("\nCategory: $category")
+    }
     println("Name: $meal")
     println("Ingredients:")
     ingredients.forEach { println(it) }
 }
 
+// 사용자로부터 식사 카테고리를 입력받는 함수
 fun getCategory(): String {
     println("Which meal do you want to add (breakfast, lunch, dinner)?")
     while (true) {
         val category = readLine()!!
-        // 입력받은 식사의 종류가 "breakfast", "lunch", "dinner" 중 하나이면, 이를 반환합니다.
         if (category in listOf("breakfast", "lunch", "dinner")) {
             return category
         }
-        // 아니라면, "Wrong meal category! Choose from: breakfast, lunch, dinner."를 출력합니다.
         println("Wrong meal category! Choose from: breakfast, lunch, dinner.")
     }
 }
 
+// 사용자로부터 식사 이름을 입력받는 함수
 fun getName(): String {
     println("Input the meal's name:")
     while (true) {
         val name = readLine()!!
-        // 입력받은 이름이 비어 있지 않고, 모든 문자가 알파벳 문자나 공백 문자이면, 이를 반환합니다.
         if (name.isNotBlank() && name.all { it.isLetter() || it.isWhitespace() }) {
             return name
         }
-        // 아니라면, "Wrong format. Use letters only!"를 출력합니다.
         println("Wrong format. Use letters only!")
     }
 }
 
+// 사용자로부터 식사 재료를 입력받는 함수
 fun getIngredients(): List<String> {
     println("Input the ingredients:")
     while (true) {
-        // 입력받은 문자열을 쉼표로 분리하고, 각 문자열의 앞뒤 공백을 제거합니다.
         val ingredients = readLine()!!.split(",").map { it.trim() }
-        // 각 재료의 이름이 비어 있지 않고, 모든 문자가 알파벳 문자나 공백 문자이면, 이를 반환합니다.
         if (ingredients.all { it.isNotBlank() && it.all { ch -> ch.isLetter() || ch.isWhitespace() } }) {
             return ingredients
         }
-        // 아니라면, "Wrong format. Use letters only!"를 출력합니다.
         println("Wrong format. Use letters only!")
     }
 }
